aa5c8d5c60e7e411d23da33dde40ef0e4797181e
Michael A <michael.aug@protonmail.com>
2020-05-03 00:19:01 +0100

    add day night cycle

---
+++ Assets/FPS/Scripts/DayNight/LightData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Lighting Preset", menuName = "Scriptables/Lighting Preset", order = 1)]
+public class LightData : ScriptableObject
+{
+    public Gradient ambientColor;
+    public Gradient directionalColor;
+    public Gradient fogColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/FPS/Scripts/DayNight/LightingManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class LightingManager : MonoBehaviour
+{
+    //References
+    [SerializeField] private Light directionalLight;
+    [SerializeField] private LightData preset;
+    //Variables
+    [SerializeField, Range(0,24)] private float timeOfDay;
+    [SerializeField] private float timeMultiplier = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Application.isPlaying)
+        {
+            timeOfDay += Time.deltaTime*timeMultiplier;
+            timeOfDay %= 24;
+            UpdateLighting(timeOfDay / 24f);
+        }
+        else
+        {
+            UpdateLighting(timeOfDay / 24f);
+        }
+    }
+
+    private void UpdateLighting(float timePercent)
+    {
+        RenderSettings.ambientLight = preset.ambientColor.Evaluate(timePercent);
+        RenderSettings.fogColor = preset.fogColor.Evaluate(timePercent);
+
+        directionalLight.color = preset.directionalColor.Evaluate(timePercent);
+        directionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 90f, 170, 0));
+    }
+}
