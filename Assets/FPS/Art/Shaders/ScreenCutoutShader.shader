3791265f609d7fa0f0c647dd9922a723f250d003
Michael A <michael.aug@protonmail.com>
2020-05-09 12:16:19 +0100

    Add Portal
    This commit adds a one-way portal in one
    of the rooms

---
+++ Assets/FPS/Art/Shaders/ScreenCutoutShader.shader
@@ -0,0 +1,63 @@
+
+Shader "Unlit/ScreenCutoutShader"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		Tags{ "Queue" = "Transparent" "IgnoreProjector" = "True" "RenderType" = "Transparent" }
+		Lighting Off
+		Cull Back
+		ZWrite On
+		ZTest Less
+		
+		Fog{ Mode Off }
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			//helper functions include
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float4 vertex : SV_POSITION;
+				float4 screenPos : TEXCOORD1;
+			};
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+
+				//transforms a point from object space to the camera’s clip space
+				o.vertex = UnityObjectToClipPos(v.vertex);
+
+				//Computes texture coordinate for doing a screenspace-mapped texture sample
+				o.screenPos = ComputeScreenPos(o.vertex);
+				return o;
+			}
+			
+			sampler2D _MainTex;
+
+			fixed4 frag (v2f i) : SV_Target
+			{
+				i.screenPos /= i.screenPos.w;
+				fixed4 col = tex2D(_MainTex, float2(i.screenPos.x, i.screenPos.y));
+				
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
---
+++ Assets/FPS/Scripts/Portal/PortalTeleporter.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTeleporter : MonoBehaviour
+{
+    public Transform player;
+    public Transform receiver;
+
+    private bool playerIsOverlapping = false;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (playerIsOverlapping)
+        {
+            Vector3 portalToPlayer = player.position - transform.position;
+            float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+
+            //if true, player moved across portal
+            if(dotProduct < 0f)
+            {
+                float rotationDiff = -Quaternion.Angle(transform.rotation, receiver.rotation);
+
+                player.Rotate(Vector3.up, rotationDiff);
+
+                Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
+
+                player.position = receiver.position + positionOffset;
+
+                playerIsOverlapping = false;
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            //Disable controller so it wouldn't reset player position while teleporting
+            player.GetComponent<PlayerCharacterController>().enabled = false;
+            playerIsOverlapping = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerIsOverlapping = false;
+
+            // re-enable controller so player could move again
+            player.GetComponent<PlayerCharacterController>().enabled = true;
+        }
+    }
+}
